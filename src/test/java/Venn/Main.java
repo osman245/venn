993e117b0a1027a7a83e0a059327dd28014024f8
jada246 <jada246@my.yorku.ca>
2020-01-13 12:38:07 -0500

    test for push

--- src/test/java/Venn/Main.java
+++ src/test/java/Venn/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println(" I am cool");
-		System.out.println("It is jada");//test for push
+		System.out.println("It is jada");//test for push part 2
 		
 		
 	}

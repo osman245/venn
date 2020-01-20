faea3090aa26b2a0d69ed1e6222c715f1ee39267
Parssa Khazra <parssak@my.yorku.ca>
2020-01-20 11:53:36 -0500

    Update Main.java

--- src/test/java/Venn/Main.java
+++ src/test/java/Venn/Main.java
@@ -2,10 +2,12 @@ package Venn;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println(" I am cool");
-		System.out.println("It is jada");//test for push part 2, more testing
-		System.out.println("123");//testtest
+	
+		
+		for(int i=0; i< 10; i++)
+		{
+			System.out.println(i);
+		}
 		
 		
 	}

70be868bca04eccc7e3324f95ca88ce365d2d28b
jada246 <jada246@gsp01.ad.eecs.yorku.ca>
2020-01-22 11:29:11 -0500

    prototype 1 - Jada

--- src/test/java/Venn/Main.java
+++ src/test/java/Venn/Main.java
@@ -1,29 +1,52 @@
 package Venn;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.awt.Graphics;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println(" I am cool");
-//<<<<<<< HEAD
-		System.out.println("It is jada");//test for push
-		System.out.println("It is war");//test for push
-		//abcss
-////=======
-		System.out.println("It is jada");//test for push part 2, more testing
-		System.out.println("123");//testtest
-//>>>>>>> branch 'master' of https://github.com/osman245/venn.git
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+	
+		//Create Border outline
+		BorderPane root = new BorderPane(); 
 		
+		//add circle coordinates x,y,r 
+		Circle cir1 = new Circle(300,300,100);
+		Circle cir2 = new Circle(450,300,100);
+    	
+		//add color to circles
+    	cir1.setFill(Color.TRANSPARENT);
+    	cir2.setFill(Color.TRANSPARENT  );
+		cir1.setStroke(Color.CORAL);
+		cir2.setStroke(Color.BLUE);
+    	
+    	//add circles to the border layout
+    	root.getChildren().add(cir1);
+    	root.getChildren().add(cir2);
+    	
+    	  
 
-		System.out.println("abcd");
-
-		for(int i = 0; i < 3; i++) {
-			System.out.println("Www");
-		}
-		System.out.println("cool cool");
-		
-		
-
-		//ydtgd
+            
+					
+			
+			
+		Scene scene = new Scene(root,800,800);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setTitle("Venn Project");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+

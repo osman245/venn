bd81799c817b77184e28b35118943ec00b4a0949
jada246 <jada246@gsp01.ad.eecs.yorku.ca>
2020-01-23 20:37:41 -0500

    jada246

---
+++ src/main/java/Venn/Main.java
@@ -0,0 +1,113 @@
+package Venn	;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Pane root = new Pane(); 
+	
+		
+		//CIRCLE1
+		Circle cir1 = new Circle(300,400,100);
+		cir1.setFill(Color.TRANSPARENT);
+		cir1.setStroke(Color.CORAL);
+		
+		//CIRCLE2
+		Circle cir2 = new Circle(450,400,100);
+		cir2.setFill(Color.TRANSPARENT);
+		cir2.setStroke(Color.BLUE);
+		
+		//NEW_VENN
+		Button new_Venn = new Button("New Venn Project");
+		new_Venn.setShape(new Circle(10));
+		new_Venn.setLayoutX(100);
+		new_Venn.setLayoutY(100);
+		
+		//CHANGETEXT
+		Button C_text = new Button("Change Text");
+		C_text.setLayoutX(100);
+		C_text.setLayoutY(700);
+		
+		//CHANGECOLOR
+		Button C_color = new Button("Change Background Color");
+		C_color.setLayoutX(300);
+		C_color.setLayoutY(700);
+		
+    	//SCENE
+    	root.getChildren().add(cir1);
+    	root.getChildren().add(cir2);
+    	root.getChildren().add(new_Venn);
+    	root.getChildren().add(C_text);
+    	root.getChildren().add(C_color);
+		Scene scene = new Scene(root,800,800);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setTitle("Venn Diagram");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		//EVENTS
+			new_Venn.setOnAction(e -> New_Venn(primaryStage));
+			C_text.setOnAction(e -> c_Text(primaryStage));
+			C_color.setOnAction(e -> c_Color(primaryStage));
+			
+				
+		}
+	private void New_Venn(Stage secondaryStage) {
+		
+		Pane root1 = new Pane();
+		Button b = new Button("press");
+		b.setLayoutY(100);
+		b.setLayoutX(100);
+		root1.getChildren().add(b);
+		Scene scene = new Scene(root1,800,800);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		secondaryStage.setTitle("New Venn Diagram");
+		secondaryStage.setScene(scene);
+		secondaryStage.show();
+	}
+	
+	private void c_Color(Stage secondaryStage) {
+		Pane root1 = new Pane();
+		//PINK
+		Button PINK = new Button("PINK");
+		PINK.setLayoutY(100);
+		PINK.setLayoutX(100);
+		root1.getChildren().add(PINK);
+		//RED
+		Button RED = new Button("RED");
+		RED.setLayoutY(100);
+		RED.setLayoutX(100);
+		root1.getChildren().add(RED);
+		//SCENE
+		Scene scene = new Scene(root1,800,800);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		secondaryStage.setTitle("New Venn Diagram");
+		secondaryStage.setScene(scene);
+		secondaryStage.show();
+		//EVENTS
+	}
+	private void c_Text(Stage secondaryStage) {
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
